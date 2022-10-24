717c5193911dd7065b1cab5971925ab8e7cc9d4b
DucTri <nguyenthanhductri.tdc2020@gmail.com>
2022-10-24 14:13:51 +0700

    home

--- app/src/main/java/com/example/appnhatro/MainActivity.java
+++ app/src/main/java/com/example/appnhatro/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.appnhatro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,17 +15,29 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     TextView a;
+    private ArrayList<String> persons = new ArrayList<String>();
+    private ViewHolderImageHome adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        a.findViewById(R.id.texx);
-        writeDatabase();
-        readDatabase();
+        setContentView(R.layout.home_tenant_layout);
+        Tri();
     }
     private void Tri(){
+//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.cardViewImageHome);
+//        persons.add("haha");
+//        persons.add("hoho");
+//        persons.add("hehe");
+//        adapter = new ViewHolderImageHome(this, R.layout.list_view_item_home_tenant, persons);
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//        recyclerView.setAdapter(adapter);
+//        adapter.notifyDataSetChanged();
     }
     //Tri
     private void writeDatabase(){
---
+++ app/src/main/java/com/example/appnhatro/ViewHolderImageHome.java
@@ -0,0 +1,56 @@
+package com.example.appnhatro;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ViewHolderImageHome extends RecyclerView.Adapter<ViewHolderImageHome.ViewHolder>{
+    private Activity context;
+    private int LayoutID;
+    private ArrayList<String> personList;
+
+    public ViewHolderImageHome(Activity context, int layoutId, ArrayList<String> personList) {
+        this.context = context;
+        this.LayoutID = layoutId;
+        this.personList = personList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View studentView =
+                inflater.inflate(R.layout.list_view_item_home_tenant, parent, false);
+        ViewHolder viewHolder = new ViewHolder(studentView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            String hinhh =personList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return personList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView hinh;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            hinh=itemView.findViewById(R.id.imageHoma);
+        }
+    }
+
+}
