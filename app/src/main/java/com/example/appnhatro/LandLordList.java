e91ea464a12167ad7ed6aa3709c10f3bcfddbfd2
vanhaust1134 <vanhaust1134@gmail.com>
2022-10-30 07:29:49 +0700

    fix some error

--- app/src/main/java/com/example/appnhatro/LandLordList.java
+++ app/src/main/java/com/example/appnhatro/LandLordList.java
@@ -2,16 +2,20 @@ package com.example.appnhatro;
 
 public class LandLordList {
     int ID,sdt,moneyhavepay;
-    String status;
+    String status,tenchutro,tentaikhoan,cmnd,diachi;
 
     public LandLordList() {
     }
 
-    public LandLordList(int ID, int sdt, int moneyhavepay, String status) {
+    public LandLordList(int ID, int sdt, int moneyhavepay, String status, String tenchutro, String tentaikhoan, String cmnd, String diachi) {
         this.ID = ID;
         this.sdt = sdt;
         this.moneyhavepay = moneyhavepay;
         this.status = status;
+        this.tenchutro = tenchutro;
+        this.tentaikhoan = tentaikhoan;
+        this.cmnd = cmnd;
+        this.diachi = diachi;
     }
 
     public int getID() {
@@ -44,5 +48,37 @@ public class LandLordList {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getTenchutro() {
+        return tenchutro;
+    }
+
+    public void setTenchutro(String tenchutro) {
+        this.tenchutro = tenchutro;
+    }
+
+    public String getTentaikhoan() {
+        return tentaikhoan;
+    }
+
+    public void setTentaikhoan(String tentaikhoan) {
+        this.tentaikhoan = tentaikhoan;
+    }
+
+    public String getCmnd() {
+        return cmnd;
+    }
+
+    public void setCmnd(String cmnd) {
+        this.cmnd = cmnd;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
     }
 }
