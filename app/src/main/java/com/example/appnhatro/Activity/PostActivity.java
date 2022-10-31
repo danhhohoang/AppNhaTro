83395506e0b8eb21b2f5a1bc04fe0fa8e7146877
danh ho <hohoangdanh.tdc2020@gmail.com>
2022-10-31 10:48:04 +0700

    them chức năng

--- app/src/main/java/com/example/appnhatro/Activity/PostActivity.java
+++ app/src/main/java/com/example/appnhatro/Activity/PostActivity.java
@@ -1,63 +1,163 @@
 package com.example.appnhatro.Activity;
 
-import static android.widget.AdapterView.*;
-
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.appnhatro.MainActivity;
 import com.example.appnhatro.R;
+import com.example.appnhatro.databinding.PostLayoutBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-public class PostActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+public class PostActivity extends AppCompatActivity{
+    PostLayoutBinding binding;
+    Uri imageUri;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
 
-    Spinner spntinhtrang;
-    Spinner spngioitinh;
-    Spinner spnsoluong;
+    Spinner spnStatus;
+    Spinner spnSex;
+    Spinner spnSl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.post_layout);
-        SpinnerSet();
+        binding = PostLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-    }
-
-    private void SpinnerSet() {
-        spntinhtrang = findViewById(R.id.spntinhtrang);
-        spngioitinh = findViewById(R.id.spngioitinh);
-        spnsoluong = findViewById(R.id.spnSl);
-
-        ArrayAdapter status = new ArrayAdapter<String>(this, R.layout.post_layout, getResources().getStringArray(R.array.string_status));
-        ArrayAdapter sex = new ArrayAdapter<String>(this, R.layout.post_layout, getResources().getStringArray(R.array.string_sex));
-        ArrayAdapter amount = new ArrayAdapter<String>(this, R.layout.post_layout, getResources().getStringArray(R.array.string_amount));
-
-        status.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
-        sex.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
-        amount.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
-        spntinhtrang.setAdapter(status);
-        spngioitinh.setAdapter(sex);
-        spnsoluong.setAdapter(amount);
-
-        spntinhtrang.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        binding.selectImagebtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(PostActivity.this, "h", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+
+
+                selectImage();
             }
         });
 
-        spngioitinh.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        binding.uploadimagebtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+
+        setSpinner();
+    }
+
+    private void setSpinner() {
+        spnStatus = findViewById(R.id.spntinhtrang);
+        spnSex = findViewById(R.id.spngioitinh);
+        spnSl = findViewById(R.id.spnSl);
+
+
+        ArrayAdapter spnStatusAdapter = new ArrayAdapter<String>(this,  androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,getResources().getStringArray(R.array.string_status));
+
+        ArrayAdapter spnSexAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.string_sex));
+
+        ArrayAdapter spnSLAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.string_amount));
+
+        spnStatus.setAdapter(spnStatusAdapter);
+        spnSex.setAdapter(spnSexAdapter);
+        spnSl.setAdapter(spnSLAdapter);
+
+        spnStatusAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spnSexAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spnSLAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
+
+        spnStatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(PostActivity.this, spnStatus.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
 
     }
+    private void uploadImage() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading File....");
+        progressDialog.show();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+        Date now = new Date();
+        String fileName = formatter.format(now);
+        storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        binding.firebaseimage1.setImageURI(null);
+                        Toast.makeText(PostActivity.this,"Successfully Uploaded",Toast.LENGTH_SHORT).show();
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+                        Toast.makeText(PostActivity.this,"Failed to Upload",Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+
+    }
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,100);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && data != null && data.getData() != null){
+
+            imageUri = data.getData();
+            binding.firebaseimage1.setImageURI(imageUri);
+
+
+        }
+    }
+
+    //
+    private String Status, address, PhoneNumber, Gender, age, arrive, price, OtherRequirements, Imageview;
+
+    public PostActivity() {
+
+    }
+    public PostActivity(String Status, String address, String PhoneNumber, String Gender, String age){
+
+    }
+
 }
