770b0fc74e42c1ce0cc0ecd192ed8546c7e45862
DucTri <nguyenthanhductri.tdc2020@gmail.com>
2022-11-01 23:05:24 +0700

    chức năng Đánh giá cập nhật ảnh

--- app/src/main/java/com/example/appnhatro/Firebase/FireBaseThueTro.java
+++ app/src/main/java/com/example/appnhatro/Firebase/FireBaseThueTro.java
@@ -2,25 +2,22 @@ package com.example.appnhatro.Firebase;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.example.appnhatro.HomeTenantActivity;
 import com.example.appnhatro.Models.BitMap;
-import com.example.appnhatro.Models.ImagePost;
 import com.example.appnhatro.Models.Post;
 import com.example.appnhatro.Models.user;
 import com.example.appnhatro.MyRecyclerViewAdapter;
-import com.example.appnhatro.Room;
 import com.example.appnhatro.TenantPostDetail;
-import com.example.appnhatro.TenantPostDetailAdapter;
+import com.example.appnhatro.item.Rating;
 import com.example.appnhatro.tool.ConverImage;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,62 +34,66 @@ import java.util.ArrayList;
 public class FireBaseThueTro {
     public FireBaseThueTro() {
     }
+
     private ConverImage converImage = new ConverImage();
-    public void docListPost( ArrayList<Post> list,MyRecyclerViewAdapter myRecyclerViewAdapter) {
+
+    public void docListPost(ArrayList<Post> list, MyRecyclerViewAdapter myRecyclerViewAdapter) {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
         databaseReference.child("Post").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     Post student = dataSnapshot.getValue(Post.class);
                     list.add(student);
                 }
                 myRecyclerViewAdapter.notifyDataSetChanged();
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
     }
-    public void readDataItem(int position,ArrayList<Post> list,Context context){
+
+    public void readDataItem(int position, ArrayList<Post> list, Context context) {
         Post data = list.get(position);
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
-                        databaseReference.child("Post").child(data.getId()).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        Intent intent = new Intent(context, TenantPostDetail.class);
-                        intent.putExtra("it_id", data.getId());
-                        intent.putExtra("it_idLogin", "KH02");
-                        context.startActivity(intent);
-                    }
+        databaseReference.child("Post").child(data.getId()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Intent intent = new Intent(context, TenantPostDetail.class);
+                intent.putExtra("it_id", data.getId());
+                intent.putExtra("it_idLogin", "KH02");
+                context.startActivity(intent);
+            }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-                    }
-                });
+            }
+        });
     }
-    public void readOnePost(Context context, String id){
+
+    public void readOnePost(Context context, String id) {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
         databaseReference.child("Post").child(id).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Post data = snapshot.getValue(Post.class);
-                BitMap bitMap = new BitMap(data.getImage(),null);
+                BitMap bitMap = new BitMap(data.getImage(), null);
                 StorageReference storageReference = FirebaseStorage.getInstance().getReference(bitMap.getTenHinh());
                 try {
-                    final File file= File.createTempFile(bitMap.getTenHinh().substring(0,bitMap.getTenHinh().length()-4),"jpg");
-                    Log.d("test", file+"");
+                    final File file = File.createTempFile(bitMap.getTenHinh().substring(0, bitMap.getTenHinh().length() - 4), "jpg");
                     storageReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
                             bitMap.setHinh(BitmapFactory.decodeFile(file.getAbsolutePath()));
-                            Log.d("test", bitMap.getHinh()+"");
-                            ((TenantPostDetail) context).setDuLieu(data,bitMap.getHinh());
+                            Log.d("test", bitMap.getHinh() + "");
+                            ((TenantPostDetail) context).setDuLieu(data, bitMap.getHinh());
                             FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
                             DatabaseReference databaseReference = firebaseDatabase.getReference();
                             databaseReference.child("user").addValueEventListener(new ValueEventListener() {
@@ -129,5 +130,42 @@ public class FireBaseThueTro {
 
             }
         });
+    }
+
+    public void getRatingPost(Context context, String idPost, String idUser) {
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        databaseReference.child("Rating").child(idPost).child(idUser).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.getValue() != null) {
+                    Rating rating = snapshot.getValue(Rating.class);
+                    ((TenantPostDetail) context).setRating(Integer.valueOf(rating.getRating()));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void addRating(String postId, String userId, String rating) {
+        Rating rating1 = new Rating(postId, userId, rating);
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("Rating");
+        databaseReference.child(postId).child(userId).setValue(rating1)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
+    }
+    public void deleteRating(String userId, String postId){
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("Rating");
+        databaseReference.child(postId).child(userId).removeValue();
     }
 }
