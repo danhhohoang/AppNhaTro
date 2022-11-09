884ba0b8f26ac02de7847aa717dad83a97564d90
theGIFTofGOD <JonathanBui.dev@gmail.com>
2022-11-09 11:13:26 +0700

    Minh: TermAndPolicy Hoan thien 95%

---
+++ app/src/main/java/com/example/appnhatro/Models/TermAndPolicy.java
@@ -0,0 +1,21 @@
+package com.example.appnhatro.Models;
+
+public class TermAndPolicy {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public TermAndPolicy() {
+    }
+
+    public TermAndPolicy(String content) {
+        this.content = content;
+    }
+}
